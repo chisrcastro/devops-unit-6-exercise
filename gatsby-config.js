5757774e91733b550ab920da1f22083836bf82b8
Shelah Castro <shelah.castro@gmail.com>
2025-10-29 12:04:37 +0800

    chore: Create new Gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Unit 6 Exercise`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
